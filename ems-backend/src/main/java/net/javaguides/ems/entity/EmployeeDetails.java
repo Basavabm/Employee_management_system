ed71a65c8487d5b2a6b97ab27b4c7ab940450960
Basavabm <basavabm30mai.com>
2026-07-11 07:51:16 +0000

    Fix EmployeeDetails column names~

--- ems-backend/src/main/java/net/javaguides/ems/entity/EmployeeDetails.java
+++ ems-backend/src/main/java/net/javaguides/ems/entity/EmployeeDetails.java
@@ -1,29 +1,18 @@
-package net.javaguides.ems.entity;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "employee_details")
 public class EmployeeDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="employee_id", nullable=false)
+    @JoinColumn(name = "employee_id", nullable = false)
     private Employee employee;
 
-    @Column(name = "key")
+    @Column(name = "detail_key")
     private String key;
 
-    @Column(name = "value")
+    @Column(name = "detail_value")
     private String value;
 }
